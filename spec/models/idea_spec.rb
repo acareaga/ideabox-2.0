2fbbe06b0a01d315b9b0052ce74ee147ee41dde4
acareaga <aaroncareaga@gmail.com>
2016-02-01 17:12:19 -0700

    Factories set and tested

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe Idea do
-
   it "has a valid factory" do
-    expect(true)
+    ideas = FactoryGirl.create(:idea 20)
+    idea = ideas.first
+
+    expect(ideas.count).to eq("")
+    expect(idea.title).to eq("Apple Computers")
+    expect(idea.body).to eq("Start a computer company in the garage.")
+    expect(idea.quality).to eq(0)
   end
 end

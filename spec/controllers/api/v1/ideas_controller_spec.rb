77fa310ab8335957aeddaff48be3a5e72f9cb568
acareaga <aaroncareaga@gmail.com>
2016-02-01 20:22:16 -0700

    Idea model is tested. Need help with idea controller tests

--- spec/controllers/api/v1/ideas_controller_spec.rb
+++ spec/controllers/api/v1/ideas_controller_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe 'index' do
+  context 'when idea is searched' do
+    xit 'returns the idea and its details on the same page' do
+      get :index
+
+      expect(response).to be_success
+    end
+  end
+end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,12 +2,26 @@ require 'rails_helper'
 
 describe Idea do
   it "has a valid factory" do
-    ideas = FactoryGirl.create(:idea 20)
+    ideas = FactoryGirl.create_list(:idea, 20)
     idea = ideas.first
 
-    expect(ideas.count).to eq("")
+    expect(ideas.count).to eq(20)
     expect(idea.title).to eq("Apple Computers")
     expect(idea.body).to eq("Start a computer company in the garage.")
+    expect(idea.quality).to eq(0)
+  end
+
+  it "can only be created with valid attributes" do
+    idea = FactoryGirl.create(:idea)
+    expect(idea).to be_valid
+
+    bad_idea = FactoryGirl.create(:idea)
+    bad_idea.quality = nil
+    expect(bad_idea).to_not be_valid
+  end
+
+  it "has a default quality of the integer zero" do
+    idea = FactoryGirl.create(:idea)
     expect(idea.quality).to eq(0)
   end
 end

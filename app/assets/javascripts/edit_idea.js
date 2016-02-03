b90d552f3806dfa99a87ad73d5419ab40bd5d1aa
acareaga <aaroncareaga@gmail.com>
2016-02-02 18:58:00 -0700

    Edit finished

--- app/assets/javascripts/edit_idea.js
+++ app/assets/javascripts/edit_idea.js
@@ -1,81 +1,47 @@
 function editIdeaTitle() {
   $('#ideas-index').delegate('#idea-title', 'keydown', function(event) {
-    event.preventDefault
-    var nl = event.which == 13
+    var enterKeyHit = event.which == 13
 
-    if (nl) {
+    if (enterKeyHit) {
       var idea = this.closest('.idea')
       var ideaId = $(idea).attr('data-id')
       var data = { title: this.textContent }
+
+      event.preventDefault();
 
       $.ajax({
         type: 'PUT',
         url: '/api/v1/ideas/' + ideaId,
         data: data,
-        success: function() {
-          // renderIndex();
-        },
+        success: function() {},
         error: function(xhr) {
           console.log(xhr.responseText)
         }
       })
     }
-
   })
 };
 
+function editIdeaBody() {
+  $('#ideas-index').delegate('#idea-body', 'keydown', function(event) {
+    var enterKeyHit = event.which == 13
 
-// document.addEventListener('keydown', function (event) {
-//   var esc = event.which == 27,
-//       nl = event.which == 13,
-//       el = event.target,
-//       input = el.nodeName != 'INPUT' && el.nodeName != 'TEXTAREA',
-//       data = {};
-//
-//   if (input) {
-//     if (esc) {
-//       // restore state
-//       document.execCommand('undo');
-//       el.blur();
-//     } else if (nl) {
-//       // save
-//       debugger;
-//       var idea_id = this.parentElement
-//       data[el.getAttribute('#ideas-index')] = el.innerHTML;
-//       // we could send an ajax request to update the field
-//       /*
-//       $.ajax({
-//         url: window.location.toString(),
-//         data: data,
-//         type: 'post'
-//       });
-//       */
-//       log(JSON.stringify(data));
-//       debugger;
-//       el.blur();
-//       event.preventDefault();
-//     }
-//   }
-// }, true);
-//
-// function log(s) {
-//   document.getElementById('debug').innerHTML = 'value changed to: ' + s;
-// };
-//
-//
-//
-// function editIdea() {
-//   $('#ideas-index').delegate('#edit-idea', 'click', function() {
-//     var idea = this.closest(".idea")
-//     $.ajax({
-//       type: 'PUT',
-//       url: '/api/v1/ideas/' + $(idea).attr('data-id'),
-//       success: function() {
-//         // $(idea).remove()
-//       },
-//       error: function(xhr) {
-//         console.log(xhr.responseText)
-//       }
-//     })
-//   })
-// };
+    if (enterKeyHit) {
+      var idea = this.closest('.idea')
+      var ideaId = $(idea).attr('data-id')
+      var data = { body: this.textContent }
+
+      event.preventDefault();
+
+      $.ajax({
+        type: 'PUT',
+        url: '/api/v1/ideas/' + ideaId,
+        data: data,
+        success: function() {},
+        error: function(xhr) {
+          console.log(xhr.responseText)
+        }
+      })
+    }
+  })
+};
--- app/assets/javascripts/ideas_index.js
+++ app/assets/javascripts/ideas_index.js
@@ -17,9 +17,9 @@ function renderIndex(idea) {
   $("#ideas-index").append(
     "<table class='centered'>"
     +"<tbody class='idea' data-id='"+ idea.id +"'>"
-      +"<td id='idea-title' contenteditable='true'>"+ idea.title +"</td>"
+      +"<td id='idea-title' contenteditable='true'><h5>"+ idea.title +"</h5></td>"
       +"<td id='idea-body' contenteditable='true'>"+ idea.body +"</td>"
-      +"<td id='idea-quality'>"+ idea.quality +""
+      +"<td id='idea-quality'><h5>"+ idea.quality +"</h5>"
       +"<td><a class='waves-effect waves-teal btn-flat' id='delete-idea'>Delete</a></td>"
     +"</tbody>"
     +"</table>"
